c5117bfa04f6207be0e8dc53fd0af4c771883924
taurarf <taurarizkia1@gmail.com>
2024-04-22 21:10:05 +0700

    -

--- resources/views/administrator/layouts/footer.blade.php
+++ resources/views/administrator/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        {{ array_key_exists('footer_app_admin', $settings) ? $settings['footer_app_admin'] : '' }} <div class="bullet"></div> Develop By <a href="https://ikhsannawawi.epizy.com/">Mochammad Ikhsan Nawawi</a>
+        {{ array_key_exists('footer_app_admin', $settings) ? $settings['footer_app_admin'] : '' }} 
     </div>
     <div class="footer-right">
 
